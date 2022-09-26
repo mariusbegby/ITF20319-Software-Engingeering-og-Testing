c2a72ea38643677e1b41c5468d55d4849bedc99b
Marius Begby <mariusbegby@gmail.com>
2022-09-26 14:14:45 +0200

    Updated to english

--- index.js
+++ index.js
@@ -2,32 +2,31 @@ const consoleArgs = process.argv.slice(2);
 let year = consoleArgs[0] || process.env.year || 2024;
 
 function isLeapYear(year) {
-  /* For å gjøre det med nøyaktig med å eliminere år delbare på 4000
-  fra å være skuddår. Da vil f.eks. ikke år 4000 eller 8000 være skuddår.*/
+  /* Divisible by 4000 should not be a leap year to make it more accurate */
   /*
   if (year % 4000 == 0) {
     return false;
   }
   */
 
-  // Dersom året er delbart på 400 er det alltid skuddår
+  // If year is divisible by 400 it is a leap year
   if (year % 400 == 0) {
     return true;
   }
 
-  // Hvis året er delbart på 4, men ikke 100, er det skuddår
+  // if year is divisible by 4, but not 100, then it is a leap year
   if (year % 4 == 0 && year % 100 != 0) {
     return true;
   }
 
-  // Alle andre år er ikke skuddår.
+  // All other years are leap years
   return false;
 }
 
 if (isLeapYear(year)) {
-  console.log(`${year} er et skuddår.`);
+  console.log(`${year} is a leap year.`);
 } else {
-  console.log(`${year} er ikke et skuddår.`);
+  console.log(`${year} is not a leap year.`);
 }
 
 exports.isLeapYear = isLeapYear;
